b8d2b936343d4b74f12dbe8cb52b9d0153cb4d5e
mcdanieltoniann <mcdanieltoniann@gmail.com>
2024-11-13 14:55:07 -0500

    ScannerDemo.java

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -6,7 +6,7 @@
 public class ScannerDemo {
 
 	// This needs to be changed !
-	private static String file1 = "C:\\Users\\mengc\\Documents\\Programming Implementation\\prog1.kay";
+	private static String file1 = "./prog1.kay";
 	private static int counter = 1;
 
 	public static void main(String args[]) {
